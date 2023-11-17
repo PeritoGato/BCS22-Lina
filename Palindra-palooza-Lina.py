40ee646c1ded87d24b0914a4ce302a3d1f60c5a7
Lina_BCS22 <123044688+PeritoGato@users.noreply.github.com>
2023-11-17 20:22:02 +0800

    Update Palindra-palooza-Lina.py

--- Palindra-palooza-Lina.py
+++ Palindra-palooza-Lina.py
@@ -1,27 +1,70 @@
 import string
 
-def is_palindrome(sentence):
-    cleaned_sentence = remove_spaces_lowercase(sentence)
-    cleaned_sentence = remove_punctuation(cleaned_sentence)
-    return is_palindrome_check(cleaned_sentence)
+class Stack:
+    def __init__(self):
+        """
+        Stack class to implement a stack data structure.
+        """
+        self.items = []  # Use a list to store stack elements
 
-def remove_spaces_lowercase(sentence):
-    return ''.join(char.lower() for char in sentence if char != ' ')
+    def push(self, data):
+        """
+        Pushes an element onto the stack.
 
-def remove_punctuation(sentence):
-    return ''.join(char for char in sentence if char not in string.punctuation)
+        Args:
+        - data: Element to be pushed onto the stack.
+        """
+        self.items.append(data)  # Append the data to the list
 
-def is_palindrome_check(sentence):
-    cleaned_sentence = remove_spaces_lowercase(sentence)
-    length = len(cleaned_sentence)
-    for i in range(length // 2):
-        if cleaned_sentence[i] != cleaned_sentence[length - 1 - i]:
-            return False
-    return True
+    def pop(self):
+        """
+        Pops an element from the stack.
 
-# Example usage:
-sentence = "A man, a plan, a canal, Panama"
-result = is_palindrome(sentence)
-print(f"The sentence '{sentence}' is a palindrome: {result}")
+        Returns:
+        - toReturn: Element popped from the stack.
+        """
+        if not self.is_empty():
+            return self.items.pop()  # Pop and return the last element from the list
+        else:
+            print("> Stack Underflow.")
+            return None
+
+    def is_empty(self):
+        """
+        Checks if the stack is empty.
+
+        Returns:
+        - bool: True if the stack is empty, False otherwise.
+        """
+        return len(self.items) == 0
+
+# Function to check if a string is a palindrome
+def is_palindrome(input_string):
+    cleaned_string = ''.join(char.lower() for char in input_string if char not in string.punctuation + ' ')
+    string_stack = Stack()
+
+    for char in cleaned_string:
+        string_stack.push(char)  # Push characters onto the stack
+
+    reversed_string = ''.join(string_stack.pop() for _ in range(len(cleaned_string)))
+
+    return cleaned_string == reversed_string  # Compare input with its reversed version
+
+# Main program
+while True:
+    user_string_input = input("\nPalindrome check!\nInput a word or sentence to check whether it's a palindrome.\n>> ").strip()
+
+    is_input_palindrome = is_palindrome(user_string_input)
+
+    print(f"\nInput: {user_string_input}")
+    if is_input_palindrome:
+        print("It's a palindrome!")
+    else:
+        print("It's NOT a palindrome!")
+
+    if input("\nTry again? (y/n)\n>> ")[0] != "y":
+        break  # Break the loop if user does not want to try again
+
+
 
 
